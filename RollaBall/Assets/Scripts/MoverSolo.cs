4e7c23d22e5069967929c56b3da117bc9a5fe58f
Mario Herrera <marioherrera0614@gmail.com>
2018-11-21 09:56:45 -0500

    proyecto base

---
+++ RollaBall/Assets/Scripts/MoverSolo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverSolo : MonoBehaviour {
+	public GameObject CuboMovible;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Movimiento");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public IEnumerator Movimiento(){
+
+		for (;;) {
+			if(Vector3.Distance (transform.position, CuboMovible.transform.position)<6){
+
+				CuboMovible.transform.position = Vector3.Lerp (CuboMovible.transform.position,
+					transform.position,
+					8f * Time.deltaTime);
+
+			}
+
+			yield return new WaitForSecondsRealtime (0.1f);
+		}
+		//StartCoroutine (Movimiento());
+	}
+}
---
+++ RollaBall/Assets/controlEnemigo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class controlEnemigo : MonoBehaviour {
+
+	Transform posicionJugador;
+	NavMeshAgent agente;
+	void Start () {
+		posicionJugador = GameObject.FindGameObjectWithTag ("Jugador").transform;
+		agente = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agente.SetDestination (posicionJugador.position);
+	}
+}
