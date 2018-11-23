d2ec9fc1054497b0a53ee2e41bc3dfe25c963cc2
Mario Herrera <marioherrera0614@gmail.com>
2018-11-22 20:53:51 -0500

    Version 3

---
+++ Proyecto Final/Assets/Proyecto/Componentes A/Scripts/Player/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public int resistencia;
+    public ParticleSystem systemaParticulas;
+    public AudioSource sonido;
+  
+
+   
+    void Start()
+    {
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void RegistrarImpacto(Vector3 puntoImpacto)
+    {
+        resistencia--;
+        systemaParticulas = systemaParticulas.GetComponent<ParticleSystem> ();
+        systemaParticulas.transform.position = puntoImpacto;
+        systemaParticulas.Play();
+        sonido.Play ();
+
+
+    }
+
+ 
+
+}
+
---
+++ Proyecto Final/Assets/Proyecto/Componentes A/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using UnitySampleAssets.CrossPlatformInput;
+using System.Collections.Generic;
+
+namespace CompleteProject
+{
+    public class PlayerShooting : MonoBehaviour
+    {
+        public int damagePerShot = 20;                  // The damage inflicted by each bullet.
+        public float timeBetweenBullets = 0.15f;        // The time between each shot.
+        public float range = 100f;                      // The distance the gun can fire.
+
+
+        float timer;                                    // A timer to determine when to fire.
+        Ray shootRay = new Ray();                       // A ray from the gun end forwards.
+        RaycastHit shootHit;                            // A raycast hit to get information about what was hit.
+        int shootableMask;                              // A layer mask so the raycast only hits things on the shootable layer.
+        ParticleSystem gunParticles;                    // Reference to the particle system.
+        LineRenderer gunLine;                           // Reference to the line renderer.
+        AudioSource gunAudio;                           // Reference to the audio source.
+        Light gunLight;                                 // Reference to the light component.
+        public Light faceLight;								// Duh
+        float effectsDisplayTime = 0.2f;                // The proportion of the timeBetweenBullets that the effects will display for.
+
+
+        void Awake()
+        {
+            // Create a layer mask for the Shootable layer.
+            shootableMask = LayerMask.GetMask("Shootable");
+
+            // Set up the references.
+            gunParticles = GetComponent<ParticleSystem>();
+            gunLine = GetComponent<LineRenderer>();
+            gunAudio = GetComponent<AudioSource>();
+            gunLight = GetComponent<Light>();
+            //faceLight = GetComponentInChildren<Light> ();
+        }
+
+
+        void Update()
+        {
+            // Add the time since Update was last called to the timer.
+            timer += Time.deltaTime;
+
+#if !MOBILE_INPUT
+            // If the Fire1 button is being press and it's time to fire...
+            if (Input.GetButton("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
+            {
+                // ... shoot the gun.
+                Shoot();
+            }
+#else
+          
+            if ((CrossPlatformInputManager.GetAxisRaw("Mouse X") != 0 || CrossPlatformInputManager.GetAxisRaw("Mouse Y") != 0) && timer >= timeBetweenBullets)
+            {
+                // ... shoot the gun
+                Shoot();
+            }
+#endif
+            // If the timer has exceeded the proportion of timeBetweenBullets that the effects should be displayed for...
+            if (timer >= timeBetweenBullets * effectsDisplayTime)
+            {
+                // ... disable the effects.
+                DisableEffects();
+            }
+        }
+
+
+        public void DisableEffects()
+        {
+            // Disable the line renderer and the light.
+            gunLine.enabled = false;
+            faceLight.enabled = false;
+            gunLight.enabled = false;
+        }
+
+
+        void Shoot()
+        {
+
+            timer = 0f;
+            gunAudio.Play();
+            gunLight.enabled = true;
+            faceLight.enabled = true;
+            gunParticles.Stop();
+            gunParticles.Play();
+            gunLine.enabled = true;
+            gunLine.SetPosition(0, transform.position);
+            shootRay.origin = transform.position;
+            shootRay.direction = transform.forward;
+
+
+            if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+            {
+
+
+                if (shootHit.collider.gameObject.CompareTag("Finish"))
+                {
+                    Debug.Log("entro");
+                    EnemyHealth resistencia = shootHit.collider.gameObject.GetComponent<EnemyHealth>();
+                    if (resistencia != null) { 
+                        resistencia.RegistrarImpacto(shootHit.point);
+                        gunLine.SetPosition(1, shootHit.point);
+                    }
+                    else { 
+                    Destroy(shootHit.collider.gameObject, 1f);
+                    }
+                }
+
+                EnemyHealth enemyHealth = shootHit.collider.gameObject.GetComponent<EnemyHealth>();
+                if (enemyHealth != null)
+                {
+                    enemyHealth.TakeDamage(damagePerShot, shootHit.point);
+                }
+
+                gunLine.SetPosition(1, shootHit.point);
+            }
+
+            else
+            {
+
+                gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+            }
+        }
+    }
+}
+   
